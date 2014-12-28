4986e690792461f0dee75e4e1708d70709088bd3
lowriek <lowriek@bc.edu>
2014-12-28 16:55:09 -0500

    More bid hooks

--- ifcrush_bid.php
+++ ifcrush_bid.php
@@ -3,7 +3,15 @@ function ifcrush_bid_show_bid_form( $frat_letters ) {
 ?>
 	<hr>
  	<form method="post">
- 		<?php  create_pnm_netIDs_menu("     "); ?> 
+ 		<?php
+ 			/* maybe a new create_available_pnms( $frat_letters )
+ 			 * with counts of the number of events attended.
+ 			 * then you can still select someone who hasn't been
+ 			 * to any of your events, but those who have are 
+ 			 * easier to find
+ 			 */  
+ 			create_pnm_netIDs_menu("     "); 
+ 		?> 
  		<input type="submit" name="action"  value="Create Bid" />
 	</form>
 	<hr>
@@ -14,4 +22,13 @@ function ifcrush_bid_handle_bid_form( $frat_letters ){
 ?>
 			<h2>Not implemented yet!  :( </h2>
 <?php
+	/* 
+	 * verify PNM bid status - 
+	 *		is unavailable if they have accepted another bid
+	 *		(i.e. isnull(status)
+	 * if the PNM has accepted another bid, they are not available
+	 */
+	
+	/* ask pnm to confirm their password */
+	/* update bid status to the frat letters of the current frat */
 }
